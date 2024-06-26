c6e15651b39c34127068a048518690c77bc4e2ac
zikn <cihorda2@gmail.com>
2024-06-26 18:41:48 +0600

    myshyk

---
+++ index.js
@@ -0,0 +1,9 @@
+
+document.querySelector('button').addEventListener('click', () => {
+    if(document.querySelector('h1').innerHTML==='Myshyk'){
+        document.querySelector('h1').innerHTML = 'aidin'
+    } else {
+        document.querySelector('h1').innerHTML = 'Myshyk'
+    }
+
+})
